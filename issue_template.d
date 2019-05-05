8c909d9659c48eb113a00e8fc6a9528e58a4a7f5
varBlu <50282441+varBlu@users.noreply.github.com>
2019-05-05 14:00:29 -0500

    Creando issue_templeate

---
+++ issue_template.d
@@ -0,0 +1,13 @@
+#Especifica el error que encontraste remplazando el texto entre comillas:
+###Navegador: 
+'CHROME'
+###Version de Website: 
+'1.0'
+###Sistema Operativo: 
+'WINDOWS'
+###Dispositivo: 
+'LAPTOP'
+###Si es un dispositivo movil ingresa el modelo: 
+'iPHONE X'
+###Explica detalladamente como ocurre el error para poder replicarlo, si tienes alguna solucion anexala:
+'DESCRIPCION'
